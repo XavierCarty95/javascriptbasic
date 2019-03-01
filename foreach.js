9582cd8525e6a2afbe6cc0283f5e7a2dc0ba562e
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-03-01 00:57:39 +0000

    for each arrays

---
+++ foreach.js
@@ -0,0 +1,20 @@
+var books = [ ]
+
+var friends = ["Oscar" , "Angela", "Kevin"]; 
+
+// for(var i = 0; i < friends.length; i++){
+//     console.log(friends[i] + "<br>")
+// }
+
+friends.forEach(function(element){
+    console.log(element + "<br>")
+})
+
+
+var Xaviers = ["Annie" , "Lawrence", "Becky"];
+
+
+
+Xaviers.forEach(function(nah){
+    console.log(nah)
+})
